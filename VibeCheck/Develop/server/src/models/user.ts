5160839e5461abb9bb7ea6b4b65ca64346874b03
Mixxuhh <wwcrase@gmail.com>
2025-02-03 22:34:46 -0700

    added liked song page, models of liked songs and User

---
+++ VibeCheck/Develop/server/src/models/user.ts
@@ -0,0 +1,68 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/database";
+import bcrypt from "bcryptjs";
+
+// Define the User attributes
+interface UserAttributes {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
+
+// Create User model class
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+
+  // Method to compare hashed passwords
+  async checkPassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
+
+// Initialize User Model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    hooks: {
+      beforeCreate: async (user: User) => {
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+    },
+  }
+);
+
+export default User;
